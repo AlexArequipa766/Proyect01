c2397461baf47fe04e7fb303b390ea9581adc6ee
markusnake121q <markusnake121q@gmail.com>
2015-07-29 03:15:46 -0500

    union de la estructura
    se termino la vivlioteca time

---
+++ 111.cpp
@@ -0,0 +1,12 @@
+ifstream horario("C:\Users\Personal\Documents\GitHub\horario.txt");
+   if (!horario())
+   {
+
+   }
+   else
+   {
+   ofstream archivo("C:\Users\Personal\Documents\GitHub\Proyecto.txt");
+	archivo<<"\n";
+	archivo.close();
+
+   }
